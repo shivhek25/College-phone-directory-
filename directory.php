dc0daa993debf483af8e19c0d84d41cdea33a00c
Shivam Singhal <shivamsinghal0610@gmail.com>
2017-07-05 16:48:15 +0530

    Commit

--- directory.php
+++ directory.php
@@ -1,10 +1,10 @@
 <?php
- $connection1=mysqli_connect("localhost:8889", "root", "root") ;
- if (!$connection1{
+ $connection=mysqli_connect("localhost:8889", "root", "root") ;
+ if (!$connection{
      die("Database Connection Failed" . mysqli_error($connection1));
  }
- $select_db1=mysqli_select_db($connection1, "College_Phone_Directory");
- if (!$select_db1){
+ $select_db=mysqli_select_db($connection, "College_Phone_Directory");
+ if (!$select_db){
      die("Database Selection Failed" . mysqli_error($connection1));
  }
  $data = mysql_query("SELECT * FROM Info");
@@ -13,3 +13,24 @@
     .", Email Address:".$row['email']."<br/>";
     }
  ?>
+ <html>
+  <head>
+    <title>College Phone Directory</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" >
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style>
+      .tabs{
+        margin: 1px solid red;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="col-md-12 tabs">
+      <div class="col-md-3">Name</div>
+      <div class="col-md-3">Position</div>
+      <div class="col-md-3">Phone Number</div>
+      <div class="col-md-3">Email</div>
+    </div>
+  </body>
+</html>
