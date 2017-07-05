94e7b787aaec4734328bbf1c0ab785cf8d31fc10
Shivam Singhal <shivamsinghal0610@gmail.com>
2017-07-05 16:52:39 +0530

    Made changes.

--- directory.php
+++ directory.php
@@ -1,21 +1,8 @@
 <?php
-<<<<<<< HEAD
- $connection=mysqli_connect("localhost:8889", "root", "root") ;
- if (!$connection{
-     die("Database Connection Failed" . mysqli_error($connection1));
- }
- $select_db=mysqli_select_db($connection, "College_Phone_Directory");
- if (!$select_db){
-     die("Database Selection Failed" . mysqli_error($connection1));
- }
- $data = mysql_query("SELECT * FROM Info");
-  while($row = mysql_fetch_assoc($data)){
-=======
  require('connect.php');
  $sql="SELECT * FROM Info";
  $data = mysqli_query($connection, $sql) or trigger_error(mysql_error()." in ".$sql);;
  while($row = mysqli_fetch_assoc($data)){
->>>>>>> 277e81e97c0b38c9699f4211d89a52309d3af462
     echo "Name ".$row['name'].", Position:".$row['position'].", Phone Number:".$row['phone_number']
     .", Email Address:".$row['email']."<br/>";
     }
